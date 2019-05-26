f5a728726927e4434f44f509ce6e2e08f5d2ec6d
Anton Yatchenko <antony3595@gmail.com>
2019-05-27 00:58:17 +0600

    изменил PurchaseVH (добавил иконки)

--- app/src/main/java/com/example/costoptimizer/adapters/viewholders/PurchaseVH.java
+++ app/src/main/java/com/example/costoptimizer/adapters/viewholders/PurchaseVH.java
@@ -3,32 +3,29 @@ package com.example.costoptimizer.adapters.viewholders;
 import android.annotation.SuppressLint;
 import android.support.annotation.NonNull;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.costoptimizer.R;
 import com.example.costoptimizer.interfaces.ClickableHolderListener;
 import com.example.costoptimizer.models.PurchaseModel;
 
-import java.text.SimpleDateFormat;
-import java.util.Locale;
-
 
 public class PurchaseVH extends ClickableViewHolder {
-    private TextView nameTextView, countTextView, costTextView, dateTW;
+    private TextView nameTextView, costTextView;
+    private ImageView iconIW;
 
     public PurchaseVH(@NonNull View itemView, ClickableHolderListener listener) {
         super(itemView, listener);
         nameTextView = itemView.findViewById(R.id.purchase_name);
-        countTextView = itemView.findViewById(R.id.purchase_count);
         costTextView = itemView.findViewById(R.id.purchase_cost);
-        dateTW = itemView.findViewById(R.id.purchase_date);
+        iconIW = itemView.findViewById(R.id.purchase_icon);
     }
 
     @SuppressLint("SimpleDateFormat")
     public void bind(PurchaseModel purchase) {
         nameTextView.setText(purchase.name);
-        countTextView.setText(String.valueOf(purchase.count));
-        costTextView.setText(String.valueOf(purchase.cost));
-        dateTW.setText(new SimpleDateFormat("dd.MM.yyyy", Locale.ENGLISH).format(purchase.date));
+        costTextView.setText("-" + String.valueOf(purchase.cost * purchase.count));
+        iconIW.setImageDrawable(this.itemView.getContext().getResources().getDrawable(purchase.category.getImage()));
     }
 }
