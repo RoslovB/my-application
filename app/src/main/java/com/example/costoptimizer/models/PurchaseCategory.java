de61f6f94cf53693f9091a6e09572389b8492842
Anton Yatchenko <antony3595@gmail.com>
2019-05-26 22:35:26 +0600

    загрузил иконки категорий покупок

--- app/src/main/java/com/example/costoptimizer/models/PurchaseCategory.java
+++ app/src/main/java/com/example/costoptimizer/models/PurchaseCategory.java
@@ -1,25 +1,32 @@
 package com.example.costoptimizer.models;
 
+import com.example.costoptimizer.R;
+
 public enum PurchaseCategory {
-    FOOD("Еда"),
-    FUN("Развлечения"),
-    CLOTHES("Одежда"),
-    SPORT("Спорт"),
-    COSMETICS("Косметика"),
-    UTILITIES("Коммунальные услуги"),
-    INTERNET("Интернет"),
-    TRANSPORT("Транспорт"),
-    OTHER("Другие");
+    FOOD("Еда", R.drawable.ic_pizza),
+    FUN("Развлечения", R.drawable.ic_popcorn),
+    CLOTHES("Одежда", R.drawable.ic_tie),
+    SPORT("Спорт", R.drawable.ic_exercise),
+    COSMETICS("Косметика", R.drawable.ic_cosmetics),
+    UTILITIES("Коммунальные услуги", R.drawable.ic_house2),
+    INTERNET("Интернет", R.drawable.ic_wifi),
+    TRANSPORT("Транспорт", R.drawable.ic_transport),
+    OTHER("Другие", R.drawable.ic_hands_and_gestures);
 
     private String name;
+    private int image;
 
-    PurchaseCategory(String name) {
-        this.name = name;
+    PurchaseCategory(String name, int image) {
+        this.name = name; this.image = image;
     }
 
     @Override
     public String toString() {
         return name;
+    }
+
+    public int getImage() {
+        return image;
     }
 
     public static PurchaseCategory fromString(String text) {
