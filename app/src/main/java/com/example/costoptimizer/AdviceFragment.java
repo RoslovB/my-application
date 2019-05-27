95b2254da8e19b47beed288032b7aed662d4a197
Anton Yatchenko <antony3595@gmail.com>
2019-05-28 00:19:11 +0600

    добавил совет (миимальная важность)

--- app/src/main/java/com/example/costoptimizer/AdviceFragment.java
+++ app/src/main/java/com/example/costoptimizer/AdviceFragment.java
@@ -6,10 +6,17 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.costoptimizer.models.PurchaseModel;
+import com.j256.ormlite.android.apptools.OpenHelperManager;
+
+import java.util.List;
 
 
 public class AdviceFragment extends Fragment {
-
+    DatabaseHelper dbHelper;
+    TextView adviceTW, descriptionTW;
 
     public AdviceFragment() {
         // Required empty public constructor
@@ -19,8 +26,35 @@ public class AdviceFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_advice, container, false);
+        dbHelper = OpenHelperManager.getHelper(getContext(), DatabaseHelper.class);
+        adviceTW = view.findViewById(R.id.advice_advice);
+        descriptionTW = view.findViewById(R.id.advice_description);
+        getAdvice();
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_advice, container, false);
+        return view;
     }
 
+    private void getAdvice() {
+        List<PurchaseModel> purchases = dbHelper.getPurchaseModelRuntimeExceptionDao().queryForAll();
+        PurchaseModel minImportancePurchase = getMinImportancePurchase(purchases);
+        String advice = String.format("Стоит задуматься, нужно ли вам было покупать \"%1$s\"?", minImportancePurchase.name);
+        String adviceDescription = String.format("Товар, с самой низкой важностью для вас является \"%s\",\n" +
+                "и его важность составляет %d, вы могли бы сэкономить %d сом", minImportancePurchase.name, minImportancePurchase.importance, (minImportancePurchase.cost * minImportancePurchase.count));
+        adviceTW.setText(advice);
+        descriptionTW.setText(adviceDescription);
+    }
+
+    private PurchaseModel getMinImportancePurchase(List<PurchaseModel> purchases) {
+        PurchaseModel minimalImportancePurchase = purchases.get(0);
+        for (PurchaseModel purchase : purchases) {
+            if (purchase.importance < minimalImportancePurchase.importance)
+                minimalImportancePurchase = purchase;
+            else if (purchase.importance == minimalImportancePurchase.importance) {
+                if ((purchase.cost * purchase.count) > (minimalImportancePurchase.cost * minimalImportancePurchase.count))
+                    minimalImportancePurchase = purchase;
+            }
+        }
+        return minimalImportancePurchase;
+    }
 }
--- app/src/main/java/com/example/costoptimizer/MainActivity.java
+++ app/src/main/java/com/example/costoptimizer/MainActivity.java
@@ -10,41 +10,30 @@ public class MainActivity extends AppCompatActivity implements HomeFragment.OnDb
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        if(findViewById(R.id.fragment_container)!=null)
-        {
-            if(savedInstanceState!=null)
-            {
+        if (findViewById(R.id.fragment_container) != null) {
+            if (savedInstanceState != null) {
                 return;
             }
 
             HomeFragment homeFragment = new HomeFragment();
-            getSupportFragmentManager().beginTransaction().add(R.id.fragment_container,homeFragment).commit();
+            getSupportFragmentManager().beginTransaction().add(R.id.fragment_container, homeFragment).commit();
         }
     }
 
     @Override
     public void dbOpPerformed(int method) {
 
-        switch (method)
-        {
+        switch (method) {
             case 0:
-
                 break;
-
-
             case 4:
-
-                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new ChartFragment()).addToBackStack(null).commit();
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new ChartFragment()).addToBackStack(null).commit();
                 break;
-
             case 5:
-
-                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new GraphFragment()).addToBackStack(null).commit();
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new GraphFragment()).addToBackStack(null).commit();
                 break;
-
             case 6:
-
-                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new AdviceFragment()).addToBackStack(null).commit();
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new AdviceFragment()).addToBackStack(null).commit();
                 break;
         }
 
