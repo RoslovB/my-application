02cd3fa4ed3e6e3e9b537f15e4281b79505cf19f
Anton Yatchenko <antony3595@gmail.com>
2019-05-29 00:03:28 +0600

    редизайн главного меню

--- app/src/main/java/com/example/costoptimizer/HomeFragment.java
+++ app/src/main/java/com/example/costoptimizer/HomeFragment.java
@@ -14,20 +14,14 @@ import android.widget.Button;
 import com.example.costoptimizer.activities.MyPurchasesActivity;
 
 
-public class HomeFragment extends Fragment  implements View.OnClickListener{
-    private Button BtnDiagram;
-    private Button BnGraph;
-    private Button BtnAdvice;
+public class HomeFragment extends Fragment implements View.OnClickListener {
     OnDbOpListener dbOpListener;
-
-
 
     public HomeFragment() {
         // Required empty public constructor
     }
 
-    public interface OnDbOpListener
-    {
+    public interface OnDbOpListener {
         public void dbOpPerformed(int method);
     }
 
@@ -37,25 +31,24 @@ public class HomeFragment extends Fragment  implements View.OnClickListener{
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_home, container, false);
-        Button btnMyPurchases = view.findViewById(R.id.btn_my_purchases);
+        View btnMyPurchases = view.findViewById(R.id.btn_my_purchases);
         btnMyPurchases.setOnClickListener(this);
 
-        BtnDiagram = view.findViewById(R.id.btn_see_diagram);
-        BtnDiagram.setOnClickListener(this);
+        View btnDiagram = view.findViewById(R.id.btn_see_diagram);
+        btnDiagram.setOnClickListener(this);
 
-        BnGraph = view.findViewById(R.id.btn_see_graph);
-        BnGraph.setOnClickListener(this);
+        View bnGraph = view.findViewById(R.id.btn_see_graph);
+        bnGraph.setOnClickListener(this);
 
-        BtnAdvice = view.findViewById(R.id.btn_advice);
-        BtnAdvice.setOnClickListener(this);
+        View btnAdvice = view.findViewById(R.id.btn_advice);
+        btnAdvice.setOnClickListener(this);
         return view;
     }
 
     @Override
     public void onClick(View view) {
 
-        switch (view.getId())
-        {
+        switch (view.getId()) {
             case R.id.btn_my_purchases:
                 Intent intent = new Intent(getContext(), MyPurchasesActivity.class);
                 startActivity(intent);
@@ -79,13 +72,11 @@ public class HomeFragment extends Fragment  implements View.OnClickListener{
         super.onAttach(context);
 
         Activity activity = (Activity) context;
-         try {
-             dbOpListener = (OnDbOpListener) activity;
-         }
-         catch (ClassCastException e)
-         {
-             throw new ClassCastException(activity.toString()+" must implement the interface method...");
-         }
+        try {
+            dbOpListener = (OnDbOpListener) activity;
+        } catch (ClassCastException e) {
+            throw new ClassCastException(activity.toString() + " must implement the interface method...");
+        }
 
-         }
+    }
 }
